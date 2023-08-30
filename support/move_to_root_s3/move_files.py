a3fb533bf0a8291a6e60063d3789d3f1dd922d32
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2023-08-30 19:39:36 +0530

    Updated the script to copy recursively

--- support/move_to_root_s3/move_files.py
+++ support/move_to_root_s3/move_files.py
@@ -2,23 +2,8 @@ import os
 import shutil
 
 root_dir = "/Users/bisshwajitsamanta/temp/archived_contents"
-
-dir_list = []
-
-for dir in os.listdir(root_dir):
-    d = os.path.join(root_dir,dir)
-    if os.path.isdir(d):
-        dir_list.append(d)
-
-for el in dir_list:
-    # List all Files
-    try:
-        all_files = os.listdir(el)
-        for files in all_files:
-            src_path = os.path.join(el, files)
-            dst_path = os.path.join(root_dir, files)
-            shutil.move(src_path, dst_path)
-
-    except FileNotFoundError:
-        print(f'File not Found exception occurred')
-        raise FileNotFoundError
+for dirpath, dirnames, filenames in os.walk(root_dir):
+    for filename in filenames:
+        src_path = os.path.join(dirpath, filename)
+        dst_path = os.path.join(root_dir, filename)
+        shutil.move(src_path, dst_path)
