b2b5899d6ab06445d0159bce2498ce19bd764242
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2023-08-30 19:58:34 +0530

    Timer added

--- support/move_to_root_s3/move_files.py
+++ support/move_to_root_s3/move_files.py
@@ -1,7 +1,11 @@
 import os
 import shutil
+from time import perf_counter
+from datetime import datetime
 
 root_dir = "/Users/bisshwajitsamanta/temp/archived_contents"
+print(f'{datetime.utcnow().isoformat()} - Starting of the script')
+start = perf_counter()
 for dirpath, dirnames, filenames in os.walk(root_dir):
     for filename in filenames:
         src_path = os.path.join(dirpath, filename)
@@ -13,3 +17,5 @@ for dirpath, dirnames, filenames in os.walk(root_dir,topdown=False):
         full_path = os.path.join(dirpath,dir)
         if not os.listdir(full_path):
             os.rmdir(full_path)
+end = perf_counter()
+print(f'{datetime.utcnow().isoformat()} - Re-organising Script Successfully completed in :: {end - start}')
