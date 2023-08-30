9393ffda89090b834676364d729f6c109be2d35f
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2023-08-30 19:49:25 +0530

    Cleaned up empty folders

--- support/move_to_root_s3/move_files.py
+++ support/move_to_root_s3/move_files.py
@@ -7,3 +7,9 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
         src_path = os.path.join(dirpath, filename)
         dst_path = os.path.join(root_dir, filename)
         shutil.move(src_path, dst_path)
+
+for dirpath, dirnames, filenames in os.walk(root_dir,topdown=False):
+    for dir in dirnames:
+        full_path = os.path.join(dirpath,dir)
+        if not os.listdir(full_path):
+            os.rmdir(full_path)
