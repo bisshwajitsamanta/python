38375d047c62b21d0e3d7402f0a6a91a92c20ff8
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2023-09-12 16:21:50 +0530

    EBS Snapshots Deletion greater than 2 year.

---
+++ udemy/aws-mastering-boto3-lambda/delete_ebs_snapshots.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta, timezone
+
+import boto3
+client = boto3.client('ec2')
+resp = client.describe_snapshots(
+    OwnerIds=['self']
+)
+for snapshot in resp['Snapshots']:
+    start_time = snapshot.get('StartTime')
+    delete_time = datetime.now(tz=timezone.utc) -timedelta(days=730)
+    if delete_time >start_time:
+        try:
+            snapshot_id =snapshot.get('SnapshotId')
+            print(f'{snapshot_id} Snapshot ID is getting deleted!')
+            client.delete_snapshot(SnapshotId=snapshot_id)
+        except Exception as ex:
+            print(f'Exception occurred:{ex}')
