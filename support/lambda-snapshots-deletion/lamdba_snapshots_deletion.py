c02fe869caf6fca210f0277d57969d47c1a5e572
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2023-09-17 12:05:28 +0530

    Got all the snapshots in a list and delete all

--- support/lambda-snapshots-deletion/lamdba_snapshots_deletion.py
+++ support/lambda-snapshots-deletion/lamdba_snapshots_deletion.py
@@ -1,7 +1,5 @@
-import json
-import os
 from datetime import datetime, timedelta, timezone
-
+import json
 import boto3
 
 client = boto3.client('ec2')
@@ -11,25 +9,19 @@ def delete_snapshots():
     resp = client.describe_snapshots(
         OwnerIds=['self']
     )
-    for snapshot in resp['Snapshots']:
-        start_time = snapshot.get('StartTime')
-        delete_time = datetime.now(tz=timezone.utc) - timedelta(minutes=int(os.environ['age']))
-        if delete_time > start_time:
-            try:
-                snapshot_id = snapshot.get('SnapshotId')
-                print(f'{snapshot_id} Snapshot ID is getting deleted!')
-                client.delete_snapshot(SnapshotId=snapshot_id)
-                return f'{snapshot_id} Snapshot ID is getting deleted!'
-
-            except Exception as ex:
-                print(f'Exception occurred:{ex}')
-                print("Skipping this snapshot")
-                continue
+    deletion_list = [snap.get('SnapshotId') for snap in resp['Snapshots'] if
+                     datetime.now(tz=timezone.utc) - timedelta(minutes=5) > snap.get('StartTime')]
+    print(f'Deletion SnapshotIds List: {deletion_list}')
+    print(f'Deletion SnapshotIds List: {len(deletion_list)}')
+    return deletion_list
 
 
 def lambda_handler(event, context):
     snapshot_ids = delete_snapshots()
+    for i in range(len(snapshot_ids)):
+        print(f"Hello You {snapshot_ids[i]} are going to be deleted")
+        client.delete_snapshot(SnapshotId=snapshot_ids[i])
     return {
         'StatusCode': 200,
-        'body': json.dumps(snapshot_ids)
+        'body': json.dumps(snapshot_ids, default=str)
     }
