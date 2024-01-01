a489672ba4bf69d3f7ef0e7ccdbea39f4ea93d54
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2024-01-01 19:49:55 +0530

    Tested in Aws account

---
+++ python_projects/rds_stop_morethan_7days/rds_lambda.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+
+rds_client = boto3.client('rds')
+
+
+def lambda_handler(event, context):
+    dbs = rds_client.describe_db_instances()
+    for db in dbs['DBInstances']:
+        try:
+            if db['DBInstanceStatus'] == 'stopped':
+                get_tags = db['TagList']
+                for tags in get_tags:
+                    if tags['Key'] == 'stop' and tags['Value'] == 'yes':
+                        print(f"Database {db['DBInstanceIdentifier']} Status is :: {db['DBInstanceStatus']}")
+            if db['DBInstanceStatus'] == 'available':
+                get_tags = db['TagList']
+                for tags in get_tags:
+                    if tags['Key'] == 'stop' and tags['Value'] == 'yes':
+                        print(f"Database {db['DBInstanceIdentifier']} Status is :: {db['DBInstanceStatus']}")
+                        response = rds_client.stop_db_cluster(DBClusterIdentifier=db['DBClusterIdentifier'])
+                        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                            print(f"Stopping Instance: {db['DBInstanceIdentifier']}")
+        except Exception as e:
+            print(f"Cannot Stop the DB Instance:{db['DBInstanceIdentifier']} with exception: {e}")
+            print("Exception:", e)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
