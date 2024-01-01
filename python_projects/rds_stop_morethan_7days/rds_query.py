9d789a009b8b272842ec016bd69fc033134e2e01
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2024-01-01 18:43:41 +0530

    RDS Code to stop

--- python_projects/rds_stop_morethan_7days/rds_query.py
+++ python_projects/rds_stop_morethan_7days/rds_query.py
@@ -6,9 +6,8 @@ rds_client = boto3.client('rds')
 def helper(db):
     get_tags = db['TagList']
     for tags in get_tags:
-        if tags['Key'] == 'autostop' and tags['Value'] == 'yes':
-            print(f"Database {db['DBInstanceIdentifier']} is {db['DBInstanceIdentifier']}")
-    pass
+        if tags['Key'] == 'stop' and tags['Value'] == 'yes':
+            print(f"Database {db['DBInstanceIdentifier']} Status is :: {db['DBInstanceStatus']}")
 
 
 def describe_rds():
@@ -16,19 +15,12 @@ def describe_rds():
     for db in dbs['DBInstances']:
         try:
             if db['DBInstanceStatus'] == 'stopped':
-                get_tags = db['TagList']
-                for tags in get_tags:
-                    if tags['Key'] == 'autostop' and tags['Value'] == 'yes':
-                        print(f"Database {db['DBInstanceIdentifier']} is Already {db['DBInstanceIdentifier']}")
-
+                helper(db)
             if db['DBInstanceStatus'] == 'available':
-                get_tags = db['TagList']
-                for tags in get_tags:
-                    if tags['Key'] == 'autostop' and tags['Value'] == 'yes':
-                        response = rds_client.stop_db_cluster(DBClusterIdentifier=db['DBClusterIdentifier'])
-                        print(f"Total Response:{response}")
-                        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                            print(f"Stopping Instance: {db['DBInstanceIdentifier']}")
+                helper(db)
+                response = rds_client.stop_db_cluster(DBClusterIdentifier=db['DBClusterIdentifier'])
+                if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                    print(f"Stopping Instance: {db['DBInstanceIdentifier']}")
         except Exception as e:
             print(f"Cannot Stop the DB Instance:{db['DBInstanceIdentifier']} with exception: {e}")
             print("Exception:", e)
